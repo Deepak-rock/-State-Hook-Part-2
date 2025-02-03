7ed9e5d614b7aaa5e3602ece9c2847f907164e30
Deepak <deepakind777@gmail.com>
2025-02-03 07:22:02 +0000

    Done

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,3 +1,6 @@
+import {useState} from 'react'
+import {v4 as uuidV4} from 'uuid'
+import CommentItem from '../CommentItem'
 import {
   CommentsContainer,
   CommentsTitle,
@@ -5,17 +8,58 @@ import {
   NameInput,
   CommentTextInput,
   CommentButton,
+  CommentsList,
 } from './styledComponents'
 
-const Comments = () => (
-  <CommentsContainer>
-    <CommentsTitle>Comments</CommentsTitle>
-    <Form>
-      <NameInput type="text" placeholder="Your name" />
-      <CommentTextInput placeholder="Your comment" rows="6" />
-      <CommentButton type="submit">Comment</CommentButton>
-    </Form>
-  </CommentsContainer>
-)
+const Comments = () => {
+  const [name, setName] = useState('')
+  const [commentText, setCommentText] = useState('')
+  const [commentsList, setCommentsList] = useState([])
 
+  const onChangeName = event => {
+    setName(event.target.value)
+  }
+
+  const onChangeCommentText = event => {
+    setCommentText(event.target.value)
+  }
+
+  const onAddComment = event => {
+    event.preventDefault()
+    const newComment = {
+      id: uuidV4(),
+      name,
+      commentText,
+    }
+    setCommentsList(prevCommentList => [...prevCommentList, newComment])
+    setName('')
+    setCommentText('')
+  }
+
+  return (
+    <CommentsContainer>
+      <CommentsTitle>Comments</CommentsTitle>
+      <Form onSubmit={onAddComment}>
+        <NameInput
+          type="text"
+          placeholder="Your name"
+          value={name}
+          onChange={onChangeName}
+        />
+        <CommentTextInput
+          placeholder="Your comment"
+          rows="6"
+          value={commentText}
+          onChange={onChangeCommentText}
+        />
+        <CommentButton type="submit">Comment</CommentButton>
+      </Form>
+      <CommentsList>
+        {commentsList.map(eachComments => (
+          <CommentItem key={eachComments.id} commentDetails={eachComments} />
+        ))}
+      </CommentsList>
+    </CommentsContainer>
+  )
+}
 export default Comments
